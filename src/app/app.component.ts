d188817f3c1b0100d818a97516a246e8e0478734
Matt Smiarowski <mbski@umich.edu>
2021-02-22 18:47:10 -0500

    something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component } from '@angular/core';
-import { AuthService } from './auth/auth.service';
-import { BehaviorSubject } from 'rxjs';
+import { Component } from "@angular/core";
+import { AuthService } from "./auth/auth.service";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  signedin$ : BehaviorSubject<boolean>;
-  username = '';
+  signedin$: BehaviorSubject<boolean>;
+  username = "";
 
   constructor(private authService: AuthService) {
     this.signedin$ = this.authService.signedin$;
+    console.log(this.signedin$);
   }
 
   ngOnInit() {

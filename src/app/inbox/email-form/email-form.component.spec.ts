9120eb11c7ce794e9070f6f5dd949c431893225d
Matt Smiarowski <mbski@umich.edu>
2021-02-23 21:37:05 -0500

    updating Angular from v10 to v11

--- src/app/inbox/email-form/email-form.component.spec.ts
+++ src/app/inbox/email-form/email-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EmailFormComponent } from './email-form.component';
 
@@ -6,7 +6,7 @@ describe('EmailFormComponent', () => {
   let component: EmailFormComponent;
   let fixture: ComponentFixture<EmailFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ EmailFormComponent ]
     })
